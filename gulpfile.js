af67c20fb8a0ae36d870bb189bb39a6194c44410
Jorge Yau <jyau01@citymail.cuny.edu>
2015-05-31 21:22:57 -0400

    Added gulpfile for build automation

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/********************************************************************
+* DEPENDENCIES
+*********************************************************************/
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var rimraf = require('rimraf');
+var strReplace = require('gulp-replace');
+
+
+/********************************************************************
+* GULP TASKS
+*********************************************************************/
+var playgroundFile = 'public/assets/js/playground.js';
+
+// [Task] removes 'src/'
+gulp.task('clean', function(cb) {
+  rimraf('src/', cb);
+});
+
+// [Task] copy public files to src
+gulp.task('src', function() {
+  gulp.src(playgroundFile)
+    .pipe(gulp.dest('src'));
+});
+
+// [Task] uglify js
+gulp.task('uglify', function() {
+  return gulp.src(playgroundFile)
+    .pipe(strReplace(/'use strict';/g, ''))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('src'));
+});
+
+// [Task] generates src
+gulp.task('default', ['src', 'uglify']);
