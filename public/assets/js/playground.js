409d9619cfaeb9f7cc29d0753acbf904ef58ccb4
Jorge Yau <jyau01@citymail.cuny.edu>
2015-06-01 08:29:15 -0400

    #4 - Mapped letter keycodes into dictionary

--- public/assets/js/playground.js
+++ public/assets/js/playground.js
@@ -60,7 +60,59 @@ Playground.prototype._initializeSettings = function() {
 };
 
 Playground.prototype._intializeKeycodes = function() {
+  // Qwerty keyboard.
   this.keycodes = {
+    'backspace':  8,
+    'tab':        9,
+    'enter':     13,
+    'shift':     16,
+    'ctrl':      17,
+    'alt':       18,
+    'caps':      20,
+    'esc':       27,
+    'space':     32,
+    'end':       35,
+    'home':      36,
+    'left':      37,
+    'up':        38,
+    'right':     39,
+    'down':      40,
+    '0':         48,
+    '1':         49,
+    '2':         50,
+    '3':         51,
+    '4':         52,
+    '5':         53,
+    '6':         54,
+    '7':         55,
+    '8':         56,
+    '9':         57,
+    'a':         65,
+    'b':         66,
+    'c':         67,
+    'd':         68,
+    'e':         69,
+    'f':         70,
+    'g':         71,
+    'h':         72,
+    'i':         73,
+    'j':         74,
+    'k':         75,
+    'l':         76,
+    'm':         77,
+    'n':         78,
+    'o':         79,
+    'p':         80,
+    'q':         81,
+    'r':         82,
+    's':         83,
+    't':         84,
+    'u':         85,
+    'v':         86,
+    'w':         87,
+    'x':         88,
+    'y':         89,
+    'z':         90,
   };
 };
 
@@ -165,6 +217,10 @@ Playground.prototype.togglePause = function() {
   }
 };
 
+Playground.prototype.getKeycode = function(key) {
+  return this.keycodes[key];
+};
+
 
 /********************************************************************
 * PUBLIC METHODS
@@ -267,14 +323,14 @@ Playground.prototype.enablePausedHUD = function() {
 Playground.prototype.keyboardInput = function(event) {
   switch (event.which) {
 
-  // spacebar: toggle pause
-  case 32:
+  // Toggle pause
+  case this.keycodes.space:
     event.preventDefault();
     this.togglePause();
     break;
 
-  // r: reset camera
-  case 82:
+  // Reset camera
+  case this.keycodes.r:
     event.preventDefault();
     this.resetCamera();
     break;
